70d0808d5a869c59b6d1446d800a9e4a1e0da032
chinhvuduc <chinhvuduc3112@hotmail.com>
2017-06-15 09:51:35 +0700

    first push

---
+++ app/src/main/java/com/vuduc/tluiot/MainActivity.java
@@ -0,0 +1,41 @@
+package com.vuduc.tluiot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+    }
+
+    private void addEvents() {
+        //Khoi tao actionbar
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addControls() {
+        mToolbar= (Toolbar) findViewById(R.id.toolbar);
+    }
+}
