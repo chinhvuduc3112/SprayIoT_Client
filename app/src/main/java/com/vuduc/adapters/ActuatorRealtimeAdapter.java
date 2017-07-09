f1c5b99740b0cbdbd3b9e869bb8fc6e306649147
chinhvuduc <chinhvuduc3112@hotmail.com>
2017-07-09 23:48:07 +0700

    Thêm màn hình Real time (tabLayout)

--- app/src/main/java/com/vuduc/adapters/ActuatorRealtimeAdapter.java
+++ app/src/main/java/com/vuduc/adapters/ActuatorRealtimeAdapter.java
@@ -1,19 +1,27 @@
 package com.vuduc.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vuduc.models.ActuatorRealtime;
 import com.vuduc.tluiot.R;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by admin on 7/8/2017.
@@ -22,26 +30,13 @@ import butterknife.BindView;
 public class ActuatorRealtimeAdapter extends RecyclerView.Adapter<ActuatorRealtimeAdapter.ViewHolder> {
 
     private Context mContext;
-    private ArrayList<ActuatorRealtime> mData;
+    private List<ActuatorRealtime> mData;
     private LayoutInflater mLayoutInflater;
 
-    public ActuatorRealtimeAdapter(Context mContext, ArrayList<ActuatorRealtime> mData) {
+    public ActuatorRealtimeAdapter(Context mContext, List<ActuatorRealtime> mData) {
         this.mContext = mContext;
-        this.mData=mData;
+        this.mData = mData;
         mLayoutInflater = LayoutInflater.from(mContext);
-    }
-
-    // Tạo viewholder hiển thị 1 item
-    @Override
-    public ActuatorRealtimeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mLayoutInflater.inflate(R.layout.item_actuator_realtime, parent, false);
-        ViewHolder holder = new ViewHolder(view);
-        return holder;
-    }
-
-    @Override
-    public void onBindViewHolder(ActuatorRealtimeAdapter.ViewHolder holder, int position) {
-        ActuatorRealtime course = mData.get(position);
     }
 
     @Override
@@ -54,9 +49,89 @@ public class ActuatorRealtimeAdapter extends RecyclerView.Adapter<ActuatorRealti
         TextView txt_actuator_name;
         @BindView(R.id.switch_actuator_realtime)
         SwitchCompat switch_actuator_realtime;
-        public ViewHolder(View itemView) {
+        @BindView(R.id.txt_actuator_time)
+        TextView txt_actuator_time;
+        @BindView(R.id.img_options)
+        ImageView img_options;
 
+        public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    // Tạo viewholder hiển thị 1 item
+    @Override
+    public ActuatorRealtimeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mLayoutInflater.inflate(R.layout.item_actuator_realtime, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ActuatorRealtimeAdapter.ViewHolder holder, int position) {
+        final ActuatorRealtime actuatorRealtime = mData.get(position);
+        holder.txt_actuator_name.setText(actuatorRealtime.getActuatorName());
+        //Chuyển đổi thời gian
+        long milis = actuatorRealtime.getTime();
+        String time = String.format("%1$d:%2$d:%3$d", TimeUnit.MILLISECONDS.toHours(milis),
+                TimeUnit.MILLISECONDS.toMinutes(milis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milis)),
+                TimeUnit.MILLISECONDS.toSeconds(milis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milis)));
+        holder.txt_actuator_time.setText(time);
+
+        if(actuatorRealtime.getStatus()){
+            holder.switch_actuator_realtime.setChecked(true);
+        }else {
+            holder.switch_actuator_realtime.setChecked(false);
+        }
+        holder.switch_actuator_realtime.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    actuatorRealtime.setStatus(true);
+                }else{
+                    actuatorRealtime.setStatus(false);
+                }
+            }
+        });
+
+        holder.img_options.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(holder.img_options);
+            }
+        });
+    }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+    private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(mContext, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_actuator_realtime, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    /**
+     * Click listener for popup menu items
+     */
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_actuator_set_time:
+                    Toast.makeText(mContext, "dfhetjnrtn", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
         }
     }
 }
--- app/src/main/java/com/vuduc/fragments/ActuatorRealtimeFragment.java
+++ app/src/main/java/com/vuduc/fragments/ActuatorRealtimeFragment.java
@@ -4,14 +4,29 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.vuduc.adapters.ActuatorRealtimeAdapter;
+import com.vuduc.models.ActuatorRealtime;
 import com.vuduc.tluiot.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class ActuatorRealtimeFragment extends Fragment {
+    @BindView(R.id.recyclerview) RecyclerView recyclerView;
+    private ActuatorRealtimeAdapter adapter;
+    private List<ActuatorRealtime> actuatorList;
+
     public ActuatorRealtimeFragment() {
         // Required empty public constructor
     }
@@ -20,6 +35,52 @@ public class ActuatorRealtimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_actuator_realtime, container, false);
+        View v =  inflater.inflate(R.layout.fragment_actuator_realtime, container, false);
+        ButterKnife.bind(this, v);
+        addEvents();
+        return v;
+    }
+
+    private void addEvents() {
+        actuatorList = new ArrayList<>();
+        adapter = new ActuatorRealtimeAdapter(getActivity().getApplicationContext(), actuatorList);
+        RecyclerView.LayoutManager mlayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(mlayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        prepareActuator();
+    }
+
+    private void prepareActuator() {
+        ActuatorRealtime a = new ActuatorRealtime("Thiết bị 1", 3600000L,true);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 2", 3600000L,true);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 3", 3600000L,false);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 4", 3600000L,true);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 5", 3600000L,false);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 6", 3600000L,true);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 7", 3600000L,false);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 8", 3600000L,true);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 9", 3600000L,false);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 10", 3600000L,true);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 11", 3600000L,false);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 12", 3600000L,true);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 13", 3600000L,false);
+        actuatorList.add(a);
+        a = new ActuatorRealtime("Thiết bị 14", 3600000L,true);
+        actuatorList.add(a);
+        adapter.notifyDataSetChanged();
     }
 }
