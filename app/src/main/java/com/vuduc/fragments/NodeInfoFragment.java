c05758ddbc446e72c8e6d7ea4824a5b5647ccfc5
chinhvuduc <chinhvuduc3112@hotmail.com>
2017-08-01 21:39:12 +0700

    hiviv

--- app/src/main/java/com/vuduc/fragments/NodeInfoFragment.java
+++ app/src/main/java/com/vuduc/fragments/NodeInfoFragment.java
@@ -4,27 +4,72 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.vuduc.adapters.ActuatorRealtimeAdapter;
 import com.vuduc.tluiot.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class NodeInfoFragment extends Fragment {
+
+    @BindView(R.id.img_options)
+    ImageView imgOption;
 
     public NodeInfoFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_node_info, container, false);
+        View v =  inflater.inflate(R.layout.fragment_node_info, container, false);
+        ButterKnife.bind(this, v);
+        addEvents();
+
+        return v;
+    }
+
+    private void addEvents() {
+        imgOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(imgOption);
+            }
+        });
+    }
+    private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(getContext(), view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_info_node, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_update_info:
+                    Toast.makeText(getContext(), "dfhetjnrtn", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
     }
 }
